0990c6c3bab1463dfd3ffb33c1b709c3a64b7f24
masharif2012 <isharifpalash@gmail.com>
2019-09-17 11:35:01 -0400

    just a simple array

---
+++ src/array/SimpleArray.java
@@ -0,0 +1,17 @@
+package array;
+
+import java.util.Scanner;
+
+public class SimpleArray {
+
+	public static void main(String[] args) {
+		int []arry =new int [] {2,3,1,5,4};
+		Scanner sc =new Scanner(System.in);
+		 int i=1;
+		 while (i<5) {
+			 System.out.println("here is the list:"+arry[i]);
+			 i++;
+		 }
+   
+	}
+}
